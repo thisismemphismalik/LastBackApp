d5daf4147412d790e530f40e1cd9a7528c354271
Memphis Malik <thisismemphismalik@gmail.com>
2022-02-10 00:18:46 +0000

    Second Page Building 🧪 Commit

---
+++ data.py
@@ -0,0 +1,93 @@
+a = {
+    "charts": {
+        "dates": {
+            "today": "12-12-2021",
+            "this_week": 25,
+            "this_month": "december",
+            "this_year": "2021",
+        },
+        "ventes": {
+            "today": 13,
+            "this_week": 13,
+            "this_month": 13,
+            "this_year": 13,
+            "all_the_time": 13
+        },
+        "nouveautés": {
+            "today": {
+                "tickets": 13,
+                "events": 13,
+            },
+            "this_week": {
+                "tickets": 13,
+                "events": 13,
+            },
+            "this_month": {
+                "tickets": 13,
+                "events": 13,
+            },
+            "this_year": {
+                "tickets": 13,
+                "events": 13,
+            },
+            "all_the_time": {
+                "tickets": 13,
+                "events": 13,
+            },
+        },
+        "utilisateurs": {
+            "today": {
+                "connexions": 13,
+                "nouveaux": 13,
+                "supprimés": 13
+            },
+            "this_week": {
+                "connexions": 13,
+                "nouveaux": 13,
+                "supprimés": 13
+            },
+            "this_month": {
+                "connexions": 13,
+                "nouveaux": 13,
+                "supprimés": 13
+            },
+            "this_year": {
+                "connexions": 13,
+                "nouveaux": 13,
+                "supprimés": 13
+            },
+            "all_the_time": {
+                "connexions": 13,
+                "nouveaux": 13,
+                "supprimés": 13
+            },
+        },
+        "finances": {
+            "today": {
+                "vendeurs": 75555,
+                "treize": 4578,
+                "orange": 455
+            },
+            "this_week": {
+                "vendeurs": 75555,
+                "treize": 4578,
+                "orange": 455
+            },
+            "this_month": {
+                "vendeurs": 75555,
+                "treize": 4578,
+                "orange": 455
+            },
+            "this_year": {
+                "vendeurs": 75555,
+                "treize": 4578,
+                "orange": 455
+            },
+            "all_the_time": {
+                "vendeurs": 75555,
+                "treize": 4578,
+                "orange": 455
+            },
+        }
+    }
+}
