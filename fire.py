9c833852d97822b35f623863775ee7d5f2021168
Memphis Malik <thisismemphismalik@gmail.com>
2022-03-25 15:53:54 +0000

    Labs Building - 🎟 Create Commit (Building Tag System)

--- fire.py
+++ fire.py
@@ -87,14 +87,27 @@ class Database:
     def add_tags(self, event_id, tags):
         all_tags = list(tags)
 
-        url = self.url + "tags" + ".json"
-        # TODO: arrays don't work
-        data = {x: [event_id] for x in all_tags}
-        data = json.dumps(data)
+        for i in all_tags:
 
-        try:
-            requests.patch(url=url, data=data)
+            url = self.url + "tags/" + i + ".json"
+            data = json.dumps({event_id: True})
 
-        except requests.exceptions.ConnectionError:
-            toast("No connection to the database")
-            Logger.warning(f"No connection to the database")
+            try:
+                requests.patch(url=url, data=data)
+
+            except requests.exceptions.ConnectionError:
+                toast("No connection to the database")
+                Logger.warning(f"No connection to the database")
+
+
+        # url = self.url + "tags" + ".json"
+        # # TODO: arrays don't work
+        # data = {x: [event_id] for x in all_tags}
+        # data = json.dumps(data)
+        #
+        # try:
+        #     requests.patch(url=url, data=data)
+        #
+        # except requests.exceptions.ConnectionError:
+        #     toast("No connection to the database")
+        #     Logger.warning(f"No connection to the database")
